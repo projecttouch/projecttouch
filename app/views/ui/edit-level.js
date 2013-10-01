9d8b97ffac1e59fb9e3b00d8b7fa09ae2982b678
Rupert Rutland <rupertrutland@gmail.com>
2013-10-01 15:37:34 +0200

    add scrubber to level edit

--- app/views/ui/edit-level.js
+++ app/views/ui/edit-level.js
@@ -15,7 +15,7 @@ define(['backbone', 'underscore'], function (Backbone, _) {
 
     return Backbone.View.extend({
         tagName: 'div',
-        className: 'media',
+        className: 'level',
 
         initialize: function () {
             _.bindAll(this, 'mouseDownRight', 'trim');
@@ -24,14 +24,17 @@ define(['backbone', 'underscore'], function (Backbone, _) {
                 end: 0
             };
             this.el.id = 'level-' + this.model.id;
+            this.levelWidth = 235;
+            this.scrubberOffset = 22;
         },
 
         render: function () {
 
-
-            this.el.style.left = '0px';
+            this.holder = document.createElement('div');
+            this.holder.className = 'holder';
+            this.holder.style.left = '0px';
             var randomStartPoint = Math.floor(Math.random() * 101);
-            this.el.style.paddingRight = randomStartPoint + 'px';
+            this.holder.style.paddingRight = randomStartPoint + 'px';
 
 
             var title = document.createElement('h1');
@@ -44,11 +47,13 @@ define(['backbone', 'underscore'], function (Backbone, _) {
 
             this.right = document.createElement('div');
             this.right.setAttribute('class', 'right');
-            this.right.style.right = (randomStartPoint - 5) + 'px';
+            this.right.style.right = (randomStartPoint - this.scrubberOffset) + 'px';
 
 //            this.el.appendChild(title);
-            this.el.appendChild(this.middle);
+            this.holder.appendChild(this.middle);
+            this.el.appendChild(this.holder);
             this.el.appendChild(this.right);
+
 
             this.hammertimeR = Hammer(this.right);
             this.hammertimeR.on("dragstart", this.mouseDownRight);
@@ -60,7 +65,7 @@ define(['backbone', 'underscore'], function (Backbone, _) {
 
         mouseDownRight: function () {
             window.App.dragging = true;
-            this.paddingRightStart = parseInt(this.el.style.paddingRight) || 0;
+            this.paddingRightStart = parseInt(this.holder.style.paddingRight) || 0;
         },
 
         trim: function (e) {
@@ -71,10 +76,10 @@ define(['backbone', 'underscore'], function (Backbone, _) {
             deltaX = e.gesture.deltaX;
             paddingRight = -deltaX + this.paddingRightStart;
             paddingRight = paddingRight < 0 ? 0 : paddingRight;
-            paddingRight = paddingRight > 260 ? 260 : paddingRight;
-            this.el.style.paddingRight = paddingRight + 'px';
-            this.$('.right').css('right', paddingRight - 2);
-            percentage = parseInt(((260 - paddingRight)/ 260) * 100);
+            paddingRight = paddingRight > this.levelWidth ? this.levelWidth : paddingRight;
+            this.holder.style.paddingRight = paddingRight + 'px';
+            this.$('.right').css('right', paddingRight - this.scrubberOffset);
+            percentage = parseInt(((this.levelWidth - paddingRight)/ this.levelWidth) * 100);
 
 //            console.log(paddingRight + 'px');
 //            console.log(percentage + '%');
