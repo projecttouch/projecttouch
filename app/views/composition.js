c46112774f889b65f422f199c2ace2c4d3ee284b
Rupert Rutland <rupertrutland@gmail.com>
2013-10-09 14:45:08 +0200

    aff footer hide in

--- app/views/composition.js
+++ app/views/composition.js
@@ -18,6 +18,8 @@ define([], function () {
             _.bindAll(this, 'togglePanels', 'toggleVideo');
             this.hammertime = Hammer(this.el);
             this.hammertime.on('doubletap', this.togglePanels);
+//            this.hammertime.on('pinchin', this.showPanels);
+//            this.hammertime.on('pinchout', this.hidePanels);
             this.hammertime.on('tap', this.toggleVideo);
         },
 
@@ -30,19 +32,29 @@ define([], function () {
         },
 
         togglePanels: function () {
-            var klass = this;
             if (this.$el.css('left') === '310px') {
-                $('#library').slideUp();
-                $('#effects').slideUp({complete: function () {
-                    klass.$el.animate({'left': 0, 'width': '100%'});
-                }});
+                this.hidePanels();
             } else {
-                var newWidth = (window.innerWidth - 620) + 'px';
-                this.$el.animate({'left': '310px', 'right': '310px', 'width': newWidth}, function () {
-                    $('#library').slideDown();
-                    $('#effects').slideDown();
-                });
+                this.showPanels();
             }
+        },
+
+        hidePanels: function () {
+            var klass = this;
+            $('#library').slideUp();
+            $('footer').animate({bottom: '-400px'});
+            $('#effects').slideUp({complete: function () {
+                klass.$el.animate({'left': 0, 'width': '100%'});
+            }});
+        },
+
+        showPanels: function () {
+            var newWidth = (window.innerWidth - 620) + 'px';
+            this.$el.animate({'left': '310px', 'right': '310px', 'width': newWidth}, function () {
+                $('footer').animate({bottom: '0px'});
+                $('#library').slideDown();
+                $('#effects').slideDown();
+            });
         }
 
     });
