fe71791c5a632228aacd9958bb860b0eb13e448a
Rupert Rutland <rupertrutland@gmail.com>
2013-10-09 13:27:42 +0200

    show/hide panel stuff

--- app/views/composition.js
+++ app/views/composition.js
@@ -14,6 +14,36 @@ define([], function () {
 
         id: 'composition',
         el: '#composition',
+        initialize: function () {
+            _.bindAll(this, 'togglePanels', 'toggleVideo');
+            this.hammertime = Hammer(this.el);
+            this.hammertime.on('doubletap', this.togglePanels);
+            this.hammertime.on('tap', this.toggleVideo);
+        },
+
+        toggleVideo: function () {
+            if (window.App.timeline.playing) {
+                window.App.timeline.stop();
+            } else {
+                window.App.timeline.play();
+            }
+        },
+
+        togglePanels: function () {
+            var klass = this;
+            if (this.$el.css('left') === '310px') {
+                $('#library').slideUp();
+                $('#effects').slideUp({complete: function () {
+                    klass.$el.animate({'left': 0, 'width': '100%'});
+                }});
+            } else {
+                var newWidth = (window.innerWidth - 620) + 'px';
+                this.$el.animate({'left': '310px', 'right': '310px', 'width': newWidth}, function () {
+                    $('#library').slideDown();
+                    $('#effects').slideDown();
+                });
+            }
+        }
 
     });
 });
