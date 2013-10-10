56b67ee4fc13906757d606f4767b06ec0e708032
Rupert Rutland <rupertrutland@gmail.com>
2013-10-10 13:14:46 +0200

    pinch stuff

--- app/views/composition.js
+++ app/views/composition.js
@@ -15,66 +15,13 @@ define([], function () {
         id: 'composition',
         el: '#composition',
         initialize: function () {
-            _.bindAll(this, 'togglePanels', 'toggleVideo', 'hidePanels', 'showPanels', 'pinch');
+            _.bindAll(this, 'togglePanels', 'toggleVideo', 'hidePanels', 'showPanels');
             this.title = this.$('h2');
             this.hammertime = Hammer(this.el);
             this.hammertime.on('doubletap', this.togglePanels);
-            this.hammertime.on('pinch', this.pinch);
             this.hammertime.on('tap', this.toggleVideo);
-            this.right = $('#level-scale .right');
-            this.holder = $('#level-scale .holder');
+
         },
-        levelWidth: null,
-        timer: null,
-        usablePixels: null,
-        startPaddingRight: null,
-        pinchThrottle: 0,
-
-        pinch: function (e) {
-            this.pinchThrottle += 1;
-            if (this.pinchThrottle < 2) {
-                return;
-            }
-            this.levelWidth = parseInt(window.App.views.edit.scaleView.levelWidth);
-
-            var klass = this,
-                newLevel,
-                right;
-            this.timer = null;
-            window.clearTimeout(this.timer);
-
-
-            if (!this.usablePixels) {
-                this.startPaddingRight = parseInt(window.App.views.edit.scaleView.holder.style.paddingRight);
-                if (this.startPaddingRight === this.levelWidth) {
-                    this.usablePixels = this.levelWidth;
-                } else {
-                    this.usablePixels = this.levelWidth - this.startPaddingRight;
-                }
-                log('usablePixels', this.usablePixels);
-            }
-
-            newLevel = this.usablePixels / e.gesture.scale;
-
-            if (newLevel > this.levelWidth) {
-                newLevel = this.levelWidth;
-            }
-            if (newLevel < 0) {
-                newLevel = 0;
-            }
-            right = newLevel - 22;
-            newLevel += 'px';
-            log('newLevel', newLevel);
-
-
-            this.holder.css('padding-right', newLevel);
-            this.right.css('right', right);
-            this.timeout = window.setTimeout(function () {
-                klass.usablePixels = null;
-            }, 1000);
-            this.pinchThrottle = 0;
-        },
-
 
         toggleVideo: function (e) {
             if (window.App.timeline.playing) {
--- app/views/ui/edit.js
+++ app/views/ui/edit.js
@@ -7,7 +7,7 @@
 /*global define, window, document, $, requirejs, require  */
 
 define(['app/views/panel',
-        'app/views/ui/edit-level'], function (Panel, LevelView) {
+    'app/views/ui/edit-level'], function (Panel, LevelView) {
 
     'use strict';
 
@@ -15,6 +15,7 @@ define(['app/views/panel',
 
         id: 'edit',
         el: '#edit',
+        active: true,
 
         events: {
             "click #btnCloseEdit": "closeEdit"
@@ -22,13 +23,69 @@ define(['app/views/panel',
 
         initialize: function () {
             Panel.prototype.initialize.call(this);
-
             this.options.collection.on("open", this.open, this);
+            _.bindAll(this, 'pinch');
+            this.right = $('#level-scale .right');
+            this.holder = $('#level-scale .holder');
+            this.scaleView = new LevelView({el: '#level-scale', type: 'scale'});
+            this.rotationView = new LevelView({el: '#level-rotation', type: 'rotation'});
+            this.volumeView = new LevelView({el: '#level-volume', type: 'volume'});
+            this.hammertime = Hammer(window.App.composition.el);
+            this.hammertime.on('pinch', this.pinch);
 
-            this.scaleView = new LevelView({el:'#level-scale', type: 'scale'});
-            this.rotationView = new LevelView({el:'#level-rotation', type:'rotation'});
-            this.volumeView = new LevelView({el:'#level-volume', type: 'volume'});
+        },
 
+        levelWidth: null,
+        timer: null,
+        usablePixels: null,
+        startPaddingRight: null,
+        pinchThrottle: 0,
+        pinching: false,
+
+        pinch: function (e) {
+            if (!this.active) {
+                return;
+            }
+            this.pinchThrottle += 1;
+            if (this.pinchThrottle < 2) {
+                return;
+            }
+            this.levelWidth = parseInt(window.App.views.edit.scaleView.levelWidth);
+
+            var klass = this,
+                newPaddingRight,
+                right;
+            if (!this.pinching) {
+                this.pinching = true;
+                this.startPaddingRight = parseInt(window.App.views.edit.scaleView.holder.style.paddingRight);
+                if (this.startPaddingRight === this.levelWidth) {
+                    this.usablePixels = this.levelWidth;
+                } else {
+                    this.usablePixels = this.levelWidth - this.startPaddingRight;
+                }
+                log('usablePixels', this.usablePixels);
+                log('this.startPaddingRight', this.startPaddingRight);
+            }
+
+            newPaddingRight = this.usablePixels / e.gesture.scale;
+
+            if (newPaddingRight > this.levelWidth) {
+                newPaddingRight = this.levelWidth;
+            }
+            if (newPaddingRight < 0) {
+                newPaddingRight = 0;
+            }
+            right = newPaddingRight - 22;
+            newPaddingRight += 'px';
+            log('newPaddingRight', newPaddingRight);
+
+
+            this.holder.css('padding-right', newPaddingRight);
+            this.right.css('right', right);
+            this.pinchThrottle = 0;
+            window.setTimeout(function () {
+                klass.pinching = false;
+            }, 2000);
         },
 
 
@@ -36,6 +93,7 @@ define(['app/views/panel',
          * ---------------------------------------------------------------------- */
 
         open: function (model) {
+            this.active = true;
             this.scaleView.model = model;
             this.rotationView.model = model;
             this.volumeView.model = model;
@@ -49,6 +107,7 @@ define(['app/views/panel',
         },
 
         closeEdit: function () {
+            this.active = false;
             $('#edit').slideUp(500, function () {
                 $('#library').slideDown(500);
             });
